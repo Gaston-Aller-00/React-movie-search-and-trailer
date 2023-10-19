c35b669a3aa174e0d0c001ceaa01fbfe0057d62b
Gaston <gastonallerdev@gmail.com>
2023-10-19 20:52:28 -0300

    add youtube , etc

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import YouTube from "react-youtube";
 import "./App.css";
+import YouTube from "react-youtube";
 
 function App() {
-  //todo lo de la API
+  // las constantes con toda la info de la api 
   const API_URL = "https://api.themoviedb.org/3";
   const API_KEY = "5a95864393a02b6e853119ef8574405e";
   const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
+
+  // endpoint para las imagenes
   const URL_IMAGE = "https://image.tmdb.org/t/p/original";
 
-  //estados
+  // los estados
   const [movies, setMovies] = useState([]);
   const [searchKey, setSearchKey] = useState("");
   const [trailer, setTrailer] = useState(null);
-  const [movie, setMovie] = useState({ tittle: "Loading movies" });
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
   const [playing, setPlaying] = useState(false);
 
-  //funcion para hacer la peticion por get  a la API
+// El modo stufita
+  const [isNightMode, setIsNightMode] = useState(false);
+
+//funcion del modo stufa
+  const toggleNightMode = () => {
+    setIsNightMode(!isNightMode);
+  };
+
+  // funcion para realizar la peticion get a la api
   const fetchMovies = async (searchKey) => {
     const type = searchKey ? "search" : "discover";
     const {
@@ -28,31 +38,43 @@ function App() {
         query: searchKey,
       },
     });
+
     setMovies(results);
     setMovie(results[0]);
-  };
-// funcion para la peticion de un solo objeto y mostrar en el reproductor
-  const fetchMovie = async(id) => {
-    const{data} = await axios.get(`${API_URL}/movie/${id}`,{
-      paramas:{
-        api_key: API_KEY,
-        append_to_response:"videos"
-      }
-    })
-    if(data.videos && data.videos.results){
-      const trailer = data.videos.results.find(
-        //si existe un offial trailer lo muestra 
-        (vid) => vid.name === "Official trailer"
-      )
-      setTrailer(trailer ? trailer : data.videos.results[0])
+
+    if (results.length) {
+      // me tira el id de la primera peli
+      await fetchMovie(results[0].id);
     }
-  }
+  };
 
+  // funcion para la peticion de un solo objeto y mostrar en reproductor de videos
+  const fetchMovie = async (id) => {
+    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "videos",
+      },
+    });
 
+    if (data.videos && data.videos.results) {
+      const trailer = data.videos.results.find(
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0]);
+    }
+    //return data
+    setMovie(data);
+  };
 
+  const selectMovie = async (movie) => {
+    fetchMovie(movie.id);
 
-  //funcion para buscar las peli
+    setMovie(movie);
+    window.scrollTo(0, 0);
+  };
 
+  // funcion para buscar las pelis
   const searchMovies = (e) => {
     e.preventDefault();
     fetchMovies(searchKey);
@@ -64,43 +86,108 @@ function App() {
 
   return (
     
-    <div className="container d-flex flex-column justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-        {/* agregar modo oscuro */}
-        {/* <button className="btn rounded-fill"><i className="bi bi-moon-fill"></i></button> */}
-        <h2 className="text-center mt-5 mb-5">Trailer Movies</h2>
-        {/* buscador de peli  */}
+    <div>
+      <h1 className="text-center mt-5 mb-5 ">Trailer  Movies</h1>
 
-        <form className="container mb-4 d-flex" onSubmit={searchMovies}>
-          <input
-            className="form-control col-auto"
-            type="text"
-            placeholder="Search movie"
-            onChange={(e) => setSearchKey(e.target.value)}
-          />
-          <button className="btn btn-primary col-auto" type="submit">
-            Search
-          </button>
-        </form>
+      {/* futuro modo noche */}
+      {/* <button onClick={toggleNightMode}> Modo stufa</button> */}
 
-        {/* div que muestra los poster de las pelis */}
-        <div className="container mt-3">
-          <div className="row">
-            {movies.map((movie) => (
-              <div key={movie.id} className="col-md-4 mb-3">
-                <img
-                  src={`${URL_IMAGE + movie.poster_path}`}
-                  alt=""
-                  height={600}
-                  width="100%"
-                />
-                <h4 className="text-center">{movie.title}</h4>
-              </div>
-            ))}
-          </div>
+
+
+
+      {/* input para buscar las pelis  */}
+      <form className="container mb-4" onSubmit={searchMovies}>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearchKey(e.target.value)}
+        />
+        <button className="btn btn-primary">Search</button>
+      </form>
+
+      <div>
+        <main>
+          {movie ? (
+            <div
+              className="viewtrailer"
+              style={{
+                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+              }}
+            >
+              {playing ? (
+                <>
+                  <YouTube
+                    videoId={trailer.key}
+                    className="reproductor container"
+                    containerClassName={"youtube-container amru"}
+                    opts={{
+                      width: "100%",
+                      height: "100%",
+                      playerVars: {
+                        autoplay: 1,
+                        controls: 0,
+                        cc_load_policy: 0,
+                        fs: 0,
+                        iv_load_policy: 0,
+                        modestbranding: 0,
+                        rel: 0,
+                        showinfo: 0,
+                      },
+                    }}
+                  />
+                  <button onClick={() => setPlaying(false)} className="boton">
+                    Close
+                  </button>
+                </>
+              ) : (
+                <div className="container">
+                  <div className="">
+                    {trailer ? (
+                      <button
+                        className="boton"
+                        onClick={() => setPlaying(true)}
+                        type="button"
+                      >
+                        Play Trailer
+                      </button>
+                    ) : (
+                      "No encontre el trailer pa, te pido disculpas"
+                    )}
+                    <h1 className="text-white">{movie.title}</h1>
+                    <p className="text-white">{movie.overview}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : null}
+        </main>
+      </div>
+
+      {/* contenedor para mostrar las pelis   en la peticion a la api */}
+      <div className="container mt-3">
+        <div className="row">
+          {movies.map((movie) => (
+            <div
+              key={movie.id}
+              className="col-md-4 mb-3"
+              onClick={() => selectMovie(movie)}
+            >
+              <img
+                src={`${URL_IMAGE + movie.poster_path}`}
+                alt=""
+                height={600}
+                width="100%"
+              />
+              <h4 className="text-center">{movie.title}</h4>
+            </div>
+          ))}
         </div>
       </div>
-    
+    </div>
   );
 }
 
 export default App;
+
+
+// algun dia si dios quiera la modularizo, hoy no

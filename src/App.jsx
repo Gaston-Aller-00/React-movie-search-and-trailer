ec2ca714d09b260a91916a77ef43ceab37d53f15
Gaston <gastonallerdev@gmail.com>
2023-10-20 21:10:34 -0300

    add animaciones opsters

--- src/App.jsx
+++ src/App.jsx
@@ -166,21 +166,22 @@ function App() {
       {/* contenedor para mostrar las pelis   en la peticion a la api */}
       <div className="container mt-3">
         <div className="row">
-          {movies.map((movie) => (
-            <div
-              key={movie.id}
-              className="col-md-4 mb-3"
-              onClick={() => selectMovie(movie)}
-            >
-              <img
-                src={`${URL_IMAGE + movie.poster_path}`}
-                alt=""
-                height={600}
-                width="100%"
-              />
-              <h4 className="text-center">{movie.title}</h4>
-            </div>
-          ))}
+        {movies.map((movie) => (
+  <div
+    key={movie.id}
+    className="col-md-4 mb-3"
+    onClick={() => selectMovie(movie)}
+  >
+    <img
+      src={`${URL_IMAGE + movie.poster_path}`}
+      alt=""
+      height={600}
+      width="100%"
+      className="movie-poster" 
+    />
+    <h4 className="text-center">{movie.title}</h4>
+  </div>
+))}
         </div>
       </div>
     </div>
